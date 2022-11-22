30b407f863cc850a4ffb74c974efa56f11b04124
Faewulf <ngolamaz3@gmail.com>
2022-11-22 19:01:42 +0700

    working on logo respon

--- scripts/index.js
+++ scripts/index.js
@@ -1,97 +1,61 @@
-console.log($(window).height() + " x " + $(window).width())
-console.log("mobile? " + isPhone())
+$(document).ready(function() {
 
-function update() {
-    $(".main_window, section.visual").css({
-        "width": "80%",
-        "height": "80%",
-        "min-width": "500px",
-        "max-width": "800px"
-    });
+  $(".social#discord").hover(
+    function() {
+      $(".title>#title-Iam").stop(true, false).animate({ "width": 'toggle' })
+      $(".title>#title-discord").stop(true, false).animate({ "width": 'toggle' })
+    },
 
-    $(".main_window, section.visual").css({
-        "top": ($(window).height() / 2 - $(".main_window").height() / 2) + "px",
-        "left": ($(window).width() / 2 - $(".main_window").width() / 2) + "px",
-        "width": "80%",
-    });
-
-
-    if (_isPhone) {
-        $(".main_window, section.visual").css({
-            "width": "90%",
-            "height": "90%",
-            "min-width": "250px",
-            "max-width": "500px"
-        });
-
-        $(".main_window, section.visual").css({
-            "top": ($(window).height() / 2 - $(".main_window").height() / 2) + "px",
-            "left": ($(window).width() / 2 - $(".main_window").width() / 2) + "px",
-        });
+    function() {
+      $(".title>#title-Iam").stop(true, false).animate({ "width": 'toggle' })
+      $(".title>#title-discord").stop(true, false).animate({ "width": 'toggle' })
     }
+  )
 
-}
-
-function projectPageInit() {
-    projects.forEach(E => {
-        let div = "<div>"
-
-        if(E.name)
-            div += `<div class="b1">${E.name}</div>`
-
-        if(E.des)
-            div += `<div>${E.des}</div>`
-
-        if(E.link)
-            div += `<a class="tlink source" href="${E.link}" target="_blank">
-    <i class="fas fa-external-link-alt" ></i>
-</a>`
-       
-        if(E.pic)
-            div += `<img src="${E.pic}" "></img>`
-
-        div += "</div>"
-
-        $("div#project>div.holder").append(div)
-    })
-}
-
-const _isPhone = isPhone()
-
-$(document).ready(function () {
-    //setTimeout(function () { body_resize() });
-    update()
-
-    if (window.location.hash == "#projects") {
-        $("#project").show()
-        $("section.terminal, a.toProjectButton").hide()
+  $(".social#youtube, .social#facebook, .social#steam").hover(
+    function() {
+      $(".title>#title-Iam").stop(true, false).animate({ "width": 'toggle' })
+    },
+    function() {
+      $(".title>#title-Iam").stop(true, false).animate({ "width": 'toggle' })
     }
+  )
 
-    if (window.location.hash == "") {
-        $("#project").hide()
-        $("section.terminal, a.toProjectButton").show()
-        console.log("ad")
+  //github
+  $(".social#github").hover(
+    function() {
+      $(".title>#title-Iam").stop(true, false).animate({ "width": 'toggle' })
+      $(".title>#title-github").stop(true, false).animate({ "width": 'toggle' })
+    },
+    function() {
+      $(".title>#title-Iam").stop(true, false).animate({ "width": 'toggle' })
+      $(".title>#title-github").stop(true, false).animate({ "width": 'toggle' })
     }
+  )
 
-    projectPageInit()
-});
+  $(".social#email").hover(
+    function() {
+      $(".title>#title-Iam").stop(true, false).animate({ "width": 'toggle' })
+      $(".title>#title-email").stop(true, false).animate({ "width": 'toggle' })
+      $(".title>#title-url").stop(true, false).animate({ "width": 'toggle' })
+    },
 
-window.onhashchange = () => {
-
-    console.log(window.location.hash)
-
-    if (window.location.hash == "#projects") {
-        $("#project").show()
-        $("section.terminal, a.toProjectButton").hide()
+    function() {
+      $(".title>#title-Iam").stop(true, false).animate({ "width": 'toggle' })
+      $(".title>#title-email").stop(true, false).animate({ "width": 'toggle' })
+      $(".title>#title-url").stop(true, false).animate({ "width": 'toggle' })
     }
+  )
 
-    if (window.location.hash == "") {
-        $("#project").hide()
-        $("section.terminal, a.toProjectButton").show()
+  $(".social#homepage").hover(
+    function() {
+      $(".title>#title-Iam").stop(true, false).animate({ "width": 'toggle' })
+      $(".title>#title-url").stop(true, false).animate({ "width": 'toggle' })
+    },
+
+    function() {
+      $(".title>#title-Iam").stop(true, false).animate({ "width": 'toggle' })
+      $(".title>#title-url").stop(true, false).animate({ "width": 'toggle' })
     }
-}
-
-$(window).on('resize', function () {
-    update()
-    //setTimeout(function () { body_resize() });
+  )
 });
